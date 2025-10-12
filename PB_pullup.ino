45521987313e039966a4280dc21111b525aa20dc
johnrafaelalegria-netizen <johnrafaelalegria@gmail.com>
2025-10-11 21:43:50 -0400

    Add files via upload

---
+++ PB_pullup.ino
@@ -0,0 +1,18 @@
+/* PB pull up
+PB pull up uses internal resistor 
+instead of having an external one
+Controlling digital input
+*/
+
+int pushButton = 13; //pin 13 is PB
+void setup(){
+pinMode(pushButton, INPUT_PULLUP); //set PB as INPUT
+//initialize communication with serial monitor
+Serial.begin(9600);
+}
+
+void loop()
+{
+  int ButtonState = digitalRead(pushButton); //read input
+  Serial.println (ButtonState);
+}
