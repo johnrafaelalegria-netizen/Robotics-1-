9c49a75cd7c3d737921286502ed05d7ae0ec9026
johnrafaelalegria-netizen <johnrafaelalegria@gmail.com>
2025-10-17 20:55:52 -0400

    Add files via upload

---
+++ Alternate_PB_RGB_CONTROL.ino
@@ -0,0 +1,31 @@
+/* Alternate PB RGB control
+when PB not pressed its RED+YELLOW
+when PB presed its BLUE+RED
+Controlling output with input component
+*/
+int pushButton = 8; //pin 8 is PB
+void setup(){
+//initialize communication with serial monitor
+  Serial.begin(9600);
+  pinMode (2,OUTPUT); //responsible for RED
+  pinMode (4,OUTPUT); //responsible BLUE
+  pinMode (12,OUTPUT); //responsible for GREEN
+  pinMode (pushButton,INPUT_PULLUP); //PB is input_pullup
+}
+void loop()
+{
+  int buttonState = digitalRead(pushButton);//read PB
+  //print out state of PB
+  Serial.print("buttonState = ");
+  Serial.print("\t") ;
+  Serial.println(buttonState); 
+  digitalWrite(12,!buttonState);//pin for GREEN
+  digitalWrite(4,buttonState);  //pin for BLUE
+  digitalWrite(2,buttonState);  //pin for RED
+  delay(1); //delay for read stability
+  //print out state of PB
+  Serial.println(buttonState);  
+  digitalWrite(12,buttonState); //pin for GREEN
+  digitalWrite(4,!buttonState); //pin for BLUE
+  digitalWrite(2,buttonState);  //pin for RED
+}
