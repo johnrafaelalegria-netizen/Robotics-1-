9eb8ca77bdda8833c1e7be04af750688f4bbb349
johnrafaelalegria-netizen <johnrafaelalegria@gmail.com>
2025-10-18 20:29:29 -0400

    RGB_color_selector (blue).ino

--- RGB_color_selector.ino
+++ RGB_color_selector.ino
@@ -1,29 +1,17 @@
-/*Color selector
-color selector
-controlling output with analog input
-*/
-
-int potentiometer = A0;
-void setup(){
-//initialize communication with serial monitor
-  Serial.begin(9600);
-  pinMode(3,OUTPUT); //pin3 is RED
-  pinMode(5,OUTPUT); //pin5 is BLUE
-  pinMode(6,OUTPUT); //pin6 is GREEN
-  pinMode(A0,INPUT); //pinA0 is potentiometer
-  
+/*Rgb color selector
+Controlling outputs*/
+// C++ code
+//
+void setup()
+{
+  pinMode(11, OUTPUT); //green pin
+  pinMode(10,OUTPUT); //blue pin
+  pinMode(9,OUTPUT); //red pin
 }
 
 void loop()
-{//Read potentiometer
-  int colorSelector = analogRead(potentiometer); 
-  //print out state of potentiometer
-  Serial.print("colorSelector =");
-  Serial.print("\t");
-  Serial.println(colorSelector);
-  
-  analogWrite(3,colorSelector); //pin for RED
-  analogWrite(5,colorSelector); //pin5 for BLUE
-  analogWrite(6,colorSelector); //pin6 for GREEN
-  delay(1); //delay for read stability
+{//color selector
+  analogWrite(11,106);  //green pin
+  analogWrite(10,228); //blue pin
+  analogWrite(9,35); //red pin
 }
