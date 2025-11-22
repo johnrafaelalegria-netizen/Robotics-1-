122017dc0aaed02c803e2fe6bc2a477b81a0a519
johnrafaelalegria-netizen <johnrafaelalegria@gmail.com>
2025-11-21 22:20:08 -0500

    Add files via upload

---
+++ PotWithBitValueDisplay.ino
@@ -0,0 +1,16 @@
+/*Potentiometer with Bit Value Display
+Read BitValue using potentiometer and display on Serial Monitor
+Analog Input processing*/
+
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+}
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(A0);
+  // print out the value you read:
+  Serial.print ("bitValue = ");
+  Serial.println (sensorValue);
+}
